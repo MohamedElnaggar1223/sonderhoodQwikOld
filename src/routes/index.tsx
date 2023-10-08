4bd268dc7f9f46ff3e8cb6a7cc1fa4b5e9d11c1f
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-10-08 19:45:24 +0300

    First Commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,104 +1,43 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useSignal, useTask$, useVisibleTask$ } from "@builder.io/qwik";
+import { useNavigate, type DocumentHead } from "@builder.io/qwik-city";
+import { useInView } from 'framer-motion'
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
+export default component$(() => 
+{
+	const navigate = useNavigate()
+	const tiltedRef = useSignal<Element>()
 
-export default component$(() => {
-  return (
-    <>
-      <Hero />
-      <Starter />
+	const isVisible = useInView(tiltedRef)
 
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
+	// useVisibleTask$(({ track }) => 
+	// {
+	// 	track(isVisible)
+	// 	track(tiltedRef)
+	// 	console.log(isVisible.value)
+	// })
 
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
+	useVisibleTask$(({ track }) => 
+	{
+		track(isVisible)
+		console.log(isVisible)
+	})
 
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
+	useVisibleTask$(() => 
+	{
+		tiltedRef.value?.scrollIntoView({behavior: 'smooth'})
+		isVisible.value = tiltedRef.value?.checkVisibility()
+	})
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div style={{ height: '300vh' }}>
+				test
+			</div>
+			<div style={{ height: '100vh' }} ref={tiltedRef}>
+				testRef
+			</div>
+		</>
+	);
 });
 
 export const head: DocumentHead = {
